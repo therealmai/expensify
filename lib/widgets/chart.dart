b4b219a7e81bd3324fc0be198400c18814931fcd
Jomar Leaño <69794504+therealmai@users.noreply.github.com>
2022-12-26 17:27:35 +0800

    31 done

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Chart extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+
+
+    );
+  }
+}
